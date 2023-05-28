509abec6223a0a52db9fefb91762327601d44c3a
Samson Ajayi <Samson.Ajayi@sterling.ng>
2023-05-28 15:40:20 +0100

    Add project files.

---
+++ RabbitReciever/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Reciever1 App";
+
+IConnection cnn = factory.CreateConnection();
+IModel channel = cnn.CreateModel();
+
+string exchangName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+
+channel.ExchangeDeclare(exchangName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+channel.QueueBind(queueName, exchangName, routingKey, arguments: null);
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Recieved: {message}");
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, autoAck: false, consumer);
+
+
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+cnn.Close();
+
+
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+IModel channel = cnn.CreateModel();
+
+string exchangName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+
+channel.ExchangeDeclare(exchangName, ExchangeType.Direct);
+channel.QueueDeclare(queueName,durable:false,exclusive:false,autoDelete:false,arguments:null);
+channel.QueueBind(queueName, exchangName, routingKey, arguments: null);
+
+
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes(s: "Hello YouTube");
+channel.BasicPublish(exchangName,routingKey,basicProperties:null,messageBodyBytes);
+
+channel.Close();
+cnn.Close();
+
+
+
+
+
+
